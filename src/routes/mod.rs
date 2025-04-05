a3a9368d86853e5efcf2068f7521e9d41fd239a8
Stepan <stepan26062001@gmail.com>
2025-04-05 20:05:37 +0300

    p. 81

---
+++ src/routes/mod.rs
@@ -0,0 +1,5 @@
+pub(crate) mod health_check;
+pub(crate) mod subscription;
+
+pub use health_check::*;
+pub use subscription::*;
---
+++ src/startup.rs
@@ -0,0 +1,18 @@
+use crate::routes::{health_check::health_check, subscription::subscribe};
+use actix_web::dev::Server;
+use actix_web::{App, HttpServer, web};
+use std::net::TcpListener;
+use sqlx::PgPool;
+
+pub fn run(listener: TcpListener, connection: PgPool) -> Result<Server, std::io::Error> {
+    let connection_pool = web::Data::new(connection);
+    let server = HttpServer::new(move || {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+            .app_data(connection_pool.clone())
+    })
+    .listen(listener)?
+    .run();
+    Ok(server)
+}
